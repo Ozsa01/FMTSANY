59235ff657f86a32665efe1818eb82824ef9da7d
Ozsa01 <onur.sahin2012@gmail.com>
2026-05-08 15:55:12 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 export default function Home() {
   return (
     <div style={{
-      minHeight:"100vh",
-      background:"linear-gradient(135deg,#b30000,#0f8a3c)",
-      color:"white",
-      padding:40
+      minHeight: "100vh",
+      background: "linear-gradient(135deg,#b30000,#0f8a3c)",
+      color: "white",
+      padding: 40
     }}>
       <h1>FMTSANY</h1>
-      <a href="/login" style={{color:"yellow"}}>
+      <p>İnsan Kaynakları Sistemi</p>
+
+      <a href="/login" style={{ color: "yellow" }}>
         Giriş Yap
       </a>
     </div>
-  )
+  );
 }
