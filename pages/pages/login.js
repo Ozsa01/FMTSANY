98a87803a927d6660adfb7f6e0882db50e01088b
Ozsa01 <onur.sahin2012@gmail.com>
2026-05-08 15:44:09 +0300

    Create login.js

---
+++ pages/pages/login.js
@@ -0,0 +1,14 @@
+export default function Login() {
+  return (
+    <div style={{padding:40}}>
+      <h1>Giriş</h1>
+
+      <input placeholder="Kullanıcı adı" />
+      <br />
+      <input type="password" placeholder="Şifre" />
+      <br /><br />
+
+      <button>Giriş Yap</button>
+    </div>
+  )
+}
